4463f181c4a18fc7e233a4a315f2172f482b0457
Al Shagoule Hamedou Bathousha <78254037+Albasha1002@users.noreply.github.com>
2022-01-10 20:03:48 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,6 @@
+var number = [1, 3, 5];
+console.log(number);
+var number1 = [1, 5, 7];
+console.log(number1);
+var a = ["basha", 12];
+console.log(a);
---
+++ array.ts
@@ -0,0 +1,9 @@
+let number: number[] = [1, 3, 5];
+
+console.log(number);
+
+let number1: Array<number> = [1, 5, 7];
+console.log(number1);
+
+let a: [string, number] = ["basha", 12];
+console.log(a);
